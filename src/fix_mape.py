fcc2d261532f20669c25f0639a7b2f0cd5eb3628
akhila-cj <cjakhila2002@gmail.com>
2026-04-05 20:53:24 +0530

    Add files via upload

---
+++ src/fix_mape.py
@@ -0,0 +1,21 @@
+import json
+
+with open('AQI_Prediction.ipynb', 'r', encoding='utf-8') as f:
+    nb = json.load(f)
+
+for cell in nb['cells']:
+    if cell['cell_type'] == 'code':
+        source = cell.get('source', [])
+        new_source = []
+        for line in source:
+            if 'nonzero_mask = y_test.values != 0' in line:
+                continue
+            if '/ y_test.values[nonzero_mask])) * 100' in line:
+                continue
+            new_source.append(line)
+        cell['source'] = new_source
+
+with open('AQI_Prediction.ipynb', 'w', encoding='utf-8') as f:
+    json.dump(nb, f, indent=1)
+
+print('Fixed!')
